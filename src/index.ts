98ac0c1535bac14c5fe95a442281c17aa7bb3359
Mamadou Talla DIA <ibntalla@gmail.com>
2026-05-14 22:25:17 +0000

    Mise à jour de la version à 0.28.6 dans package.json et réorganisation de set-globals avec ajout de nouvelles fonctions utilitaires.

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
+export * from "./set-globals";
 export * from "./config";
 export * from "./user";
 export * from "./job";
@@ -9,14 +10,3 @@ export * from "./doc";
 export * from "./currency";
 export * from "./types";
 export * from "./utils";
-
-import type { ApplyDataOptions } from "./apply_data";
-
-declare global {
-  // @ts-ignore
-  var _: typeof import("lodash");
-  var z: typeof import("zod");
-
-  var apiBaseUrl: string;
-  var applyDataOptionsList: (ApplyDataOptions & { key: string })[];
-}
--- src/set-globals.ts
+++ src/set-globals.ts
@@ -1,5 +1,44 @@
 import lodash from "lodash";
 import * as zod from "zod";
+import { ApplyDataOptions } from "./apply_data";
 
-globalThis._ = lodash;
-globalThis.z = zod;
+if (!globalThis._) {
+  globalThis._ = lodash;
+
+  _.isArrayString = isArrayString;
+  _.sleep = sleep;
+
+  function isArrayString(value: string) {
+    try {
+      const array = JSON.parse(value) as any[];
+      return Array.isArray(array);
+    } catch {
+      return false;
+    }
+  }
+
+  async function sleep(time = 500) {
+    await new Promise((resolve) => setTimeout(resolve, time));
+  }
+}
+
+if (!globalThis.z) globalThis.z = zod;
+
+declare global {
+  // @ts-ignore
+  var _: typeof import("lodash");
+  var z: typeof import("zod");
+
+  var apiBaseUrl: string;
+  var applyDataOptionsList: (ApplyDataOptions & { key: string })[];
+}
+
+declare module "lodash" {
+  interface LoDashStatic {
+    isArrayString(value?: any): boolean;
+    sleep(
+      /** @default 500 */
+      time?: number
+    ): Promise<void>;
+  }
+}
